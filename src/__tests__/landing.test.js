04c5cec53c43741df8d9680e668ffcf05e72ba69
TmothyMartel <t.m.martel@gmail.com>
2018-09-09 10:00:28 -0400

    added smoke tests

---
+++ src/__tests__/landing.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import Landing from "../components/landing/landing";
+
+describe("<Landing />", () => {
+	it("Renders without crashing", () => {
+		shallow(<Landing />);
+	});
+});
